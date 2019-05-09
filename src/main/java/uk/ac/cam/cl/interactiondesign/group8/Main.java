8fa536ad66b962f5923ba1d751809bc6eb09870c
Jake Hillion <jake@hillion.co.uk>
2019-05-09 15:38:16 +0100

    Fixed code

--- src/main/java/uk/ac/cam/cl/interactiondesign/group8/Main.java
+++ src/main/java/uk/ac/cam/cl/interactiondesign/group8/Main.java
@@ -1,13 +1,7 @@
 package uk.ac.cam.cl.interactiondesign.group8;
 
-import uk.ac.cam.cl.interactiondesign.group8.api.DummyLocationProvider;
-import uk.ac.cam.cl.interactiondesign.group8.api.OpenWeatherMapAPI;
-import uk.ac.cam.cl.interactiondesign.group8.api.WeatherAPI;
-
 public class Main {
     public static void main(String[] args) {
-        WeatherAPI api = new OpenWeatherMapAPI(new DummyLocationProvider());
 
-        System.out.println(api.getCurrentTemperature());
     }
 }
