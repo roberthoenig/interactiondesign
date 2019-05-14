287c150847554cd2dcfa5776c71e75f9b0bc6e7e
James Hogge <jameshogge@gmail.com>
2019-05-14 20:17:04 +0100

    Added A Cheeky UnitConverter

--- src/main/java/uk/ac/cam/cl/interactiondesign/group8/ui/WindSock.java
+++ src/main/java/uk/ac/cam/cl/interactiondesign/group8/ui/WindSock.java
@@ -8,21 +8,26 @@ import java.awt.image.*;
 public class WindSock extends JImage {
 	protected Scene scene;
 
-	private static final int IMAGE_COUNT = 4;
+	private static final float[] WIND_THRESHOLDS = new float[] { 1.6f, 5.5f, 10.8f };
 	private BufferedImage[] windSockImages;
-	private int windSpeed;
+	private float windSpeed;
 
-	public void setWindSpeed(int wind)
+	public void setWindSpeed(float wind)
 	{
 		windSpeed = wind;
+
+		// Switch to corresponding image
+		int img = 0;
+		while (wind > WIND_THRESHOLDS[img]) ++img;
+		setImage(windSockImages[img]);
 	}
 
 	public WindSock(Scene s) {
 		scene = s;
 
 		try {
-			windSockImages = new BufferedImage[IMAGE_COUNT];
-			for (int i = 0; i < IMAGE_COUNT; ++i)
+			windSockImages = new BufferedImage[WIND_THRESHOLDS.length + 1];
+			for (int i = 0; i <= WIND_THRESHOLDS.length; ++i)
 				windSockImages[i] = ResourceLoader.loadImage("scenecomponents/windsock" + Integer.toString(i) + ".png");
 		}
 		catch (IOException e) {
---
+++ src/main/java/uk/ac/cam/cl/interactiondesign/group8/utils/UnitConverter.java
@@ -0,0 +1,110 @@
+package uk.ac.cam.cl.interactiondesign.group8.utils;
+
+import java.text.*;
+
+public class UnitConverter {
+	public static enum Temp { K, C, F };
+    public static enum Velocity { MPH, KMH, MS };
+
+    // Converts a temperature between two units
+    public static float convertTemp(Temp from, Temp to, float value)
+    {
+        float kelvin;
+        switch (from)
+        {
+        case K:
+            kelvin = value;
+            break;
+        case C:
+            kelvin = value + 273.15f;
+            break;
+        case F:
+            kelvin = (value + 459.67f) * 0.555555556f;
+            break;
+        default:
+            throw new RuntimeException("Invalid unit");
+        }
+
+        switch (to)
+        {
+        case K:
+            return kelvin;
+        case C:
+            return value - 273.15f;
+        case F:
+            return (kelvin / 0.555555556f) - 459.67f;
+        default:
+            throw new RuntimeException("Invalid unit");
+        }
+    }
+
+    // Converts temperature to a printable string
+    public static String tempToString(Temp units, float value)
+    {
+        DecimalFormat format = new DecimalFormat("#");
+        String result = format.format(value);
+
+        switch (units)
+        {
+        case K:
+            return result + "K";
+        case C:
+            return result + "\u00B0C";
+        case F:
+            return result + "\u00B0F";
+        default:
+            throw new RuntimeException("Invalid unit");
+        }
+    }
+
+    // Converts a velocity between two units
+    public static float convertVel(Velocity from, Velocity to, float value)
+    {
+        float ms;
+        switch (from)
+        {
+        case MPH:
+            ms = value / 2.23694f;
+            break;
+        case KMH:
+            ms = value / 3.6f;
+            break;
+        case MS:
+            ms = value;
+            break;
+        default:
+            throw new RuntimeException("Invalid unit");
+        }
+
+        switch (to)
+        {
+        case MPH:
+            return ms * 2.23694f;
+        case KMH:
+            return ms * 3.6f;
+        case MS:
+            return ms;
+        default:
+            throw new RuntimeException("Invalid unit");
+        }
+    }
+
+    // Converts velocity to a printable string
+    public static String velToString(Velocity units, float value)
+    {
+        DecimalFormat format = new DecimalFormat("#");
+        String result = format.format(value);
+
+        switch (units)
+        {
+        case MPH:
+            return result + "mph";
+        case KMH:
+            return result + "km/h";
+        case MS:
+            return result + "m/s";
+        default:
+            throw new RuntimeException("Invalid unit");
+        }
+    }
+}
