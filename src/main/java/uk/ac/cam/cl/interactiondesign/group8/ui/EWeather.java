5e4931560571efcc158f7fb74b44ff18d4637d71
Jake Hillion <JakeHillion2@users.noreply.github.com>
2019-05-21 13:06:57 +0100

    Speech bubble text from localisation file

--- src/main/java/uk/ac/cam/cl/interactiondesign/group8/ui/EWeather.java
+++ src/main/java/uk/ac/cam/cl/interactiondesign/group8/ui/EWeather.java
@@ -1,13 +1,29 @@
 package uk.ac.cam.cl.interactiondesign.group8.ui;
 
-public enum EWeather {
-    THUNDERSTORM,    // codes 200-299
-    DRIZZLE,        // codes 300-399
-    RAIN,            // codes 500-599
-    SNOW,            // codes 600-699
+public enum EWeather implements IChatInterface {
+    THUNDERSTORM(3),    // codes 200-299
+    DRIZZLE(1),             // codes 300-399
+    RAIN(2),                   // codes 500-599
+    SNOW(3),                   // codes 600-699
 
-    FOG,            // code 741
+    FOG(2),                     // code 741
 
-    OVERCAST,        // code 804
-    CLEAR            // any codes not already mentioned
+    OVERCAST(1),           // code 804
+    CLEAR(2);                  // any codes not already mentioned
+
+    private double probability;
+
+    EWeather(double probability) {
+        this.probability = probability;
+    }
+
+    @Override
+    public double getProbability() {
+        return probability;
+    }
+
+    @Override
+    public String getHeader() {
+        return "weather";
+    }
 }
---
+++ src/main/java/uk/ac/cam/cl/interactiondesign/group8/ui/IChatInterface.java
@@ -0,0 +1,6 @@
+package uk.ac.cam.cl.interactiondesign.group8.ui;
+
+public interface IChatInterface {
+    double getProbability();
+    String getHeader();
+}
